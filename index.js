cdeacabc73a460de3e93e248563ae249fc9bf05d
gcchaan <gcchaan@users.noreply.github.com>
2018-03-21 16:42:38 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+const BbPromise = require('bluebird');
+const path = require("path");
+const child_process_1 = require("child_process");
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.hooks = {
+      'package:compileFunctions': () => BbPromise.bind(this).then(this.mergeRsources),
+    };
+  }
+  mergeRsources() {
+    const templatePath = this.serverless.service.custom.cloudform.resources;
+    const data = child_process_1.execSync(`$(find node_modules -type l -name ts-node) -e "import t from '${path.resolve(templatePath)}'; console.log(JSON.stringify(t))"`, { encoding: 'utf-8' });
+    const res = JSON.parse(data);
+    if (!this.serverless.service.resources) {
+      this.serverless.service.resources = {};
+    }
+    if (!this.serverless.service.resources.Resources) {
+      this.serverless.service.resources.Resources = {};
+    }
+    Object.assign(this.serverless.service.resources.Resources, res);
+  }
+}
+
+module.exports = ServerlessPlugin;
